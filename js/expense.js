0c044980a8e69a40790d06c395a54ba23c9845b6
Sehrish <sehrishzeba012@gmail.com>
2022-08-27 19:41:04 +0600

    4th

--- js/expense.js
+++ js/expense.js
@@ -12,6 +12,7 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
 
     //set player expense
     setTextElementvalueById('player-expenses', totalPlayerExpense);
+
 })
 
 document.getElementById('btn-calculate-total').addEventListener('click', function () {
